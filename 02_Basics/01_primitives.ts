d8d72599ad541d5c97bd0142f487b759d1cefecd
Silson sapkota <silson007@gmail.com>
2023-05-22 22:58:20 +0545

    Add: Primitives

---
+++ 02_Basics/01_primitives.ts
@@ -0,0 +1,15 @@
+// The primitives: string, number and boolean
+
+// In TypeScript, there are three primary primitive types: string, number, and boolean. These types represent basic data types that are commonly used in programming. Let's explore each of these types in detail:
+
+// - String
+let message: string = "Hello, TypeScript!";
+
+// - Number
+let total: number = 100;
+
+// - Boolean
+let isLogged: boolean = true;
+
+// Here we assigned a value to a variable using the colon (:) followed by the type. This is called a type annotation.
+// The type annotation is used by the TypeScript compiler to analyze the code for type correctness.
