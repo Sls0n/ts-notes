c2dcbfd13cd0dd7fe02f43ca4b4217057d07b283
Silson sapkota <silson007@gmail.com>
2023-05-22 23:00:05 +0545

    Add: Type Inference

---
+++ 02_Basics/02_type-inference.ts
@@ -0,0 +1,9 @@
+// Type Inference
+
+// TypeScript has a powerful type inference system that can deduce the type of a variable based on its assigned value. In many cases, you don't need to explicitly annotate the type, as TypeScript can infer it. For example:
+
+let greeting = "Hello, TypeScript!"; // Inferred type: string
+let temperature = 25; // Inferred type: number
+let isAvailable = true; // Inferred type: boolean
+
+// In the above example, TypeScript automatically infers the appropriate types based on the assigned values.
